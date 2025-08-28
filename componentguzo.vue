2c4bcdb1f3ce85bf245db4bad7389fa1a2564d04
junhyeon0325 <jjh990325@gmail.com>
2025-08-29 01:46:14 +0900

    20250829 0146

---
+++ componentguzo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>componentguzo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'componentGuzo',
+  components: {
+    
+  },
+  data() {
+    return {
+    };
+  },
+  setup() {
+    return {};
+  },
+  created() {
+  },  
+  mounted() {
+  },
+  unmounted() {
+  },
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/binding">Databinding</router-link>
-  </nav>
+  </nav>  
   <router-view />
 </template>
 
